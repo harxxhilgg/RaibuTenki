dfe2b4254a9de522b56d0a989a4f480c1ab3e32f
Harshil Patel <harshilp046@gmail.com>
2025-01-06 21:55:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="rounded-[48px] border-b-4 border-slate-300 text-white flex justify-center align-middle p-5 text-[30px] italic mx-20">
+        <h1>Something</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
