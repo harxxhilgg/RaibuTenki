7de91b5a2d41497259a14882791df60e069592bc
Harshil Patel <harshilp046@gmail.com>
2025-01-08 00:59:39 +0530

    feat: added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="rounded-[48px] border-b-4 border-slate-300 text-white flex justify-center align-middle p-5 text-[30px] mx-20 font-bold select-none">
+      <h1>RaibuTenki</h1>
+    </div>
+  );
+};
+
+export { Header };
