67c3c5038d72e8a92930039c57b727b1b907bccd
Harshil Patel <harshilp046@gmail.com>
2025-01-08 00:51:25 +0530

    feat: fetching data from api, if true, then passing data as props to WeatherInfo with error (if any) and passed a function as props to SearchBar component to get city, if yes, data, else error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-function App() {
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { SearchBar } from "./components/SearchBar";
+import { WeatherInfo } from "./components/WeatherInfo";
+
+const App = () => {
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  const fetchWeather = async (city) => {
+    setError("");
+    setWeather(null);
+
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+
+      if (!res.ok) {
+        throw new Error("City not found.");
+      }
+
+      const data = await res.json();
+      setWeather(data);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <>
-      <div className="rounded-[48px] border-b-4 border-slate-300 text-white flex justify-center align-middle p-5 text-[30px] italic mx-20">
-        <h1>Something</h1>
-      </div>
+      <Header />
+      <SearchBar onSearch={fetchWeather} />
+      <WeatherInfo weather={weather} error={error} />
     </>
   );
-}
+};
 
 export default App;
